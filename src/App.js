a45ca9b7e9c60b26d7c7507bada65a366e692cf7
pnevmat <pnevmat001@gmail.com>
2022-08-30 14:53:36 +0300

    adds mlp marcup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import swHeader from './images/sw-header.webp';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img src={swHeader} alt="Star Wars" />
+      </div>
+      <div>
+        <h1>Watch yout favorite movies and TV shows</h1>
+        <p>
+          Stream unlimited movies and TV shows on your pc, tablet, mobile and TV
+        </p>
+        <a href="#">Get started!</a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
